586891d6ec3f6044cbf8dc0041e74d17a482e1af
Christy <cjj9000@gmail.com>
2019-07-11 17:01:58 -0400

    christy's work: added function starts

---
+++ functionstart.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+def use_mic():
+    from pathlib import Path
+    '''
+    Records audio for 10 seconds using the microphone and checks against song data in database to identify song
+    
+    Parameters
+    ------------
+    None
+    
+    Returns
+    ------------
+    
+    '''
+    from microphone import record_audio
+    frames, sampling_rate = record_audio(10)
+    listen_audio = np.hstack([np.frombuffer(i, np.int16) for i in frames])
+    return listen_audio.shape
+    
+def test(song_path):
+    from pathlib import Path
+    '''
+    Checks audio file against existing song database to identify song
+    Parameters
+    ------------
+    song_path: Path object
+    File Path of test song
+    
+    Returns
+    ------------
+    
+    
+    '''
+    
+    
+    import numpy as np
+    import librosa
+    samples, fs = librosa.load(str(song_path), sr=44100, mono=True)
+    samples*=2**15
+    return samples.shape
+    
+
+def add_songs(song_path, name = None, artist = None):
+    from pathlib import Path
+    '''
+    Inputs song path of audio file, as well as optional keyword arguments for strings which name the song and the artist
+    Generates data from the audio file, then manipulates data to generate keys
+    Assigns keys to song and adds to database
+    Parameters
+    ------------
+    song_path: Path object
+    File Path of test song
+    
+    name: string
+    name of song
+    
+    artist: string
+    name of artist
+    
+    
+    Returns
+    ------------
+    
+    
+    '''
+
+    import numpy as np
+    import librosa
+    samples, fs = librosa.load(str(song_path), sr=44100, mono=True)
+    samples*=2**15
+    return samples.shape
+    
+song_root = Path(r"C:\Users\jaimo\Downloads\Cogworks Songs")
+local_song_path = song_root / r"Eminem - Stan (Long Version) ft. Dido.mp3"
+test(local_song_path)
