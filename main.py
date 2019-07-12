e697c0c8389fb451e7639f64f257d36b79579d2f
Christy <cjj9000@gmail.com>
2019-07-12 16:14:47 -0400

    barely changed

--- main.py
+++ main.py
@@ -9,16 +9,20 @@ import spectrogram
 from pathlib import Path
 import pickle
     
-def path_to_db(filename, window_size, song_name, artist_name):
+def path_to_db(filepath, window_size, song_name, artist_name):
 
-    samples = functionstart.add_songs(filename)
+    samples = functionstart.add_songs(filepath)
     spec_test = spectrogram.spec_creator(samples)
     cutoff = background_def.back_val_finder(spec_test)
     peaks = peak_finding_code.local_peaks(spec_test, cutoff, window_size)
     pickle_in = open("song_data.pickle", "rb")
     song_data = pickle.load(pickle_in)
-    n = str(int(sorted(song_data.keys())[-1])+1)
-    song_data[n] = (song_name, artist_name)
+    if song_data.keys():
+        n = str(int(sorted(song_data.keys())[-1])+1)
+        song_data[n] = (song_name, artist_name)
+    else:
+        n = 1
+        song_data[n] = (song_name, artist_name)
     fp = create_val_db.create_database(peaks, n)
     populate_db.populate_db(fp)
 
