4bc8afa136dfb541ad7c1e8cac76134a8c3e1c55
ValianceX <sydney.karimi02@gmail.com>
2019-07-12 10:12:28 -0400

    added comments

--- get_song.py
+++ get_song.py
@@ -44,8 +44,9 @@ def get_song(fingerprint):
                 offset = time_bin -  match[1]
                 matches.append((match[0], offset))
 
-    top_three = c.most_common(3)
+    top_three = c.most_common(3) # gets tuples with the top three most common based on matches
 
+    # returns either the song data or "No Song Found"
     if abs(top_three[0][1] - top_three[1][1]) > 5 and top_three[0][1] > 5:
         song_title = song_data[top_three[0][0]][0]
         song_artist = song_data[top_three[0][0]][1]
